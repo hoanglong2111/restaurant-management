1d525ebbdf6a82e690140d503eb9c37d79151999
hoanglong <vulehoanglong123@gmail.com>
2025-11-21 13:36:36 +0700

    Fix menuRoute.js by removing markdown code block markers that were causing TypeError

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -1,79 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const MenuItem = require('../models/menuItem');
+const menuController = require('../controllers/menuController');
 const { protect, isAdmin } = require('../middleware/authMiddleware');
+const validate = require('../middleware/validationMiddleware');
+const { menuSchemas } = require('../utils/validationSchemas');
 
 // Tạo món ăn mới
-router.post('/create', protect, isAdmin, async (req, res) => {
-    const { name, description, price, category, imageUrls, stock, sold } = req.body;
-
-    try {
-        const menuItem = new MenuItem({
-            name,
-            description,
-            price,
-            category,
-            imageUrls,
-            stock,
-            sold: sold || 0,
-        });
-
-        const createdMenuItem = await menuItem.save();
-        res.status(201).json(createdMenuItem);
-    } catch (error) {
-        res.status(400).json({ message: 'Unable to create menu item.' });
-    }
-});
+router.post('/create', protect, isAdmin, validate(menuSchemas.createItem), menuController.createMenuItem);
 
 // Lấy danh sách tất cả các món ăn
-router.get('/', async (req, res) => {
-    try {
-        const menuItems = await MenuItem.find({});
-        res.json(menuItems);
-    } catch (error) {
-        res.status(400).json({ message: 'Unable to retrieve menu items.' });
-    }
-});
+router.get('/', menuController.getAllMenuItems);
 
 // Cập nhật món ăn
-router.put('/:id', protect, isAdmin, async (req, res) => {
-    try {
-        const menuItem = await MenuItem.findById(req.params.id);
-
-        if (menuItem) {
-            menuItem.name = req.body.name || menuItem.name;
-            menuItem.description = req.body.description || menuItem.description;
-            menuItem.price = req.body.price || menuItem.price;
-            menuItem.category = req.body.category || menuItem.category;
-            menuItem.imageUrls = req.body.imageUrls || menuItem.imageUrls;
-            menuItem.availability = req.body.availability !== undefined ? req.body.availability : menuItem.availability;
-            menuItem.stock = req.body.stock !== undefined ? req.body.stock : menuItem.stock;
-            menuItem.sold = req.body.sold !== undefined ? req.body.sold : menuItem.sold;
-
-            const updatedMenuItem = await menuItem.save();
-            res.json(updatedMenuItem);
-        } else {
-            res.status(404).json({ message: 'Menu item not found' });
-        }
-    } catch (error) {
-        res.status(400).json({ message: 'Unable to update menu item.' });
-    }
-});
+router.put('/:id', protect, isAdmin, validate(menuSchemas.updateItem), menuController.updateMenuItem);
 
 // Xóa món ăn
-router.delete('/:id', protect, isAdmin, async (req, res) => {
-    try {
-        const menuItem = await MenuItem.findById(req.params.id);
-
-        if (menuItem) {
-            await menuItem.remove();
-            res.json({ message: 'Menu item removed' });
-        } else {
-            res.status(404).json({ message: 'Menu item not found' });
-        }
-    } catch (error) {
-        res.status(400).json({ message: 'Unable to delete menu item.' });
-    }
-});
+router.delete('/:id', protect, isAdmin, menuController.deleteMenuItem);
 
 module.exports = router;
